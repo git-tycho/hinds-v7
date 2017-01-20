10bc77615dd60fdacc201256f1a22b2f8271cd17
Terence Bennell <tycho@Terences-MacBook-Pro.local>
2017-01-19 23:21:54 -0700

    swap in ng-book form

--- src/apply-form.component.ts
+++ src/apply-form.component.ts
@@ -1,55 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { User } from './apply.interface';
+import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup
+} from '@angular/forms';
 
 @Component({
-  selector: 'apply-form',
+  selector: 'demo-form-sku-builder',
   template: `
-    <form novalidate (ngSubmit)="onSubmit(user)" [formGroup]="user">
-      <label>
-        <span>Job Reference:</span>
-        <input type="text" formControlName="jobid">
-      </label>
-      <label>
-        <span>Full name</span>
-        <input type="text" placeholder="Your full name" formControlName="name">
-      </label>
-      <div class="error" *ngIf="user.get('name').touched && user.get('name').hasError('required')">
-        Name is required
-      </div>
-      <div class="error" *ngIf="user.get('name').touched && user.get('name').hasError('minlength')">
-        Minimum of 2 characters
-      </div>
-      <label>
-        <span>Email address</span>
-        <input type="email" placeholder="Your email address" formControlName="email">
-      </label>
-      <div
-        class="error"
-        *ngIf="user.get('account').get('email').hasError('required') && user.get('account').get('email').touched">
-        Email is required
-      </div>
-      <label>
-        <span>Phone number</span>
-        <input type="number" placeholder="Your phone number" formControlName="phone">
-      </label>
+  <div class="ui raised segment">
+    <h2 class="ui header">Demo Form: Sku with Builder</h2>
+    <form [formGroup]="myForm"
+          (ngSubmit)="onSubmit(myForm.value)"
+          class="ui form">
 
-      <button type="submit" [disabled]="user.invalid">Submit</button>
+      <div class="field">
+        <label for="skuInput">SKU</label>
+        <input type="text"
+               id="skuInput"
+               placeholder="SKU"
+               [formControl]="myForm.controls['sku']">
+      </div>
+
+    <button type="submit" class="ui button">Submit</button>
     </form>
+  </div>
   `
 })
-export class ApplyFormComponent implements OnInit {
-  user: FormGroup;
-  constructor(private fb: FormBuilder) {}
-  ngOnInit() {
-    this.user = this.fb.group({
-      jobid: ['22222'],
-      name: ['', [Validators.required, Validators.minLength(2)]],
-      email: ['', Validators.required],
-      phone: ['', Validators.required]
+export class DemoFormSkuBuilder {
+  myForm: FormGroup;
+
+  constructor(fb: FormBuilder) {
+    this.myForm = fb.group({
+      'sku': ['ABC123']
     });
   }
-  onSubmit({ value, valid }: { value: User, valid: boolean }) {
-    console.log(value, valid);
+
+  onSubmit(value: string): void {
+    console.log('you submitted value: ', value);
   }
 }
